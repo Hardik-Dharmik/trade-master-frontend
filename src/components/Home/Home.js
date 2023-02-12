255b0c46c68522c9361cde40b0e2ea2e748d0a3d
Hardik <hardikdharmik94@gmail.com>
2023-02-12 13:47:52 +0530

    Footer completed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -32,13 +32,13 @@ function Home() {
 
       <p className="text-2xl my-1 text-gray-600">Practice now, Earn tommorow</p>
 
-      <div className="flex my-4 bg-gray-200 w-3/5 md:w-2/5  items-center rounded-lg py-2">
+      <div className="flex my-4 bg-gray-200 w-fit md:w-2/5  items-center rounded-lg py-2">
         <input
           type="text"
           placeholder="Search Company"
           className="bg-gray-200 flex-grow focus:outline-none mx-4 px-2 text-base"
         />
-        <MagnifyingGlassIcon className="hidden sm:flex h-5 w-5 mx-2 text-gray-600" />
+        <MagnifyingGlassIcon className=" h-5 w-5 mx-2 text-gray-600" />
       </div>
 
       <div className="flex flex-wrap items-center space-x-3 md:w-2/5 w-3/5 gap-y-3 justify-center">
